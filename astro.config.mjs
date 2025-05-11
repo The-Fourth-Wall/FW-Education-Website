ac9d69dd6f1be8ab824a4e71ea8d634bd1c5a05d
oblivious <atha@fourth-wall.org>
2025-05-11 04:23:45 +0300

    [master] - Added svelte.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import {defineConfig} from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [svelte()],
   srcDir: "./src/client",
 });
