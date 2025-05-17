b2a52291d63bfc2b48499d9145dc38a67c910fa1
oblivious <atha@fourth-wall.org>
2025-05-17 16:32:56 +0300

    [master] - modified the back button to perform a history.back() and reimplemented most <a> tag behaviours.

---
+++ src/client/02-molecules/BackButtonNavigation.svelte
@@ -0,0 +1,17 @@
+<script>
+  import {onMount} from "svelte";
+
+  onMount(() => {
+    const button = document.getElementById("02-molecules/BackButton.astro");
+
+    button?.addEventListener("click", () => {
+      history.back();
+    });
+
+    button?.addEventListener("keydown", event => {
+      if (event.key === "Enter") {
+        history.back();
+      }
+    });
+  });
+</script>
