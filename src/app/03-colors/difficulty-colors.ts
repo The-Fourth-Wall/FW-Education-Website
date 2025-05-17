35f8f2243f5acc2b9c9fda47bd9f79e25049f1a0
oblivious <atha@fourth-wall.org>
2025-05-18 02:56:27 +0300

    [master] - added more contrast to the colors.

--- src/app/03-colors/difficulty-colors.ts
+++ src/app/03-colors/difficulty-colors.ts
@@ -4,20 +4,20 @@ export function get_difficulty_accent(difficulty: Difficulty) {
   return {
     fundamental: "color-mix(in srgb, var(--green) 90%, var(--white))",
     beginner: "color-mix(in srgb, var(--blue) 80%, var(--white))",
-    intermediate: "var(--orange)",
+    intermediate: "color-mix(in srgb, var(--orange) 90%, var(--black))",
     advanced: "color-mix(in srgb, var(--crimson) 85%, var(--white))",
-    expert: "color-mix(in srgb, var(--indigo) 75%, var(--white))",
-    master: "color-mix(in srgb, var(--onyx) 60%, var(--white))",
+    expert: "color-mix(in srgb, var(--indigo) 70%, var(--white))",
+    master: "color-mix(in srgb, var(--onyx) 65%, var(--white))",
   }[difficulty];
 }
 
 export function get_difficulty_background(difficulty: Difficulty) {
   return {
-    fundamental: "color-mix(in srgb, var(--green) 80%, var(--black))",
+    fundamental: "color-mix(in srgb, var(--green) 55%, var(--black))",
     beginner: "color-mix(in srgb, var(--blue) 85%, var(--black))",
-    intermediate: "color-mix(in srgb, var(--orange) 78%, var(--black))",
-    advanced: "color-mix(in srgb, var(--crimson) 85%, var(--black))",
-    expert: "var(--indigo)",
+    intermediate: "color-mix(in srgb, var(--orange) 70%, var(--black))",
+    advanced: "color-mix(in srgb, var(--crimson) 70%, var(--black))",
+    expert: "color-mix(in srgb, var(--indigo) 95%, var(--white))",
     master: `linear-gradient(
       0deg,
       color-mix(in srgb, var(--onyx) 80%, var(--white)) 0%,
