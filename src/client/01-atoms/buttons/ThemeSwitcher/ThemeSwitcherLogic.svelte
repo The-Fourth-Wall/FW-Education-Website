fa4620daeb844871711183b4d8d193153f80e2a9
oblivious <atha@fourth-wall.org>
2025-05-18 22:44:31 +0300

    [master] - added the UI for a theme switcher.

---
+++ src/client/01-atoms/buttons/ThemeSwitcher/ThemeSwitcherLogic.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import {onMount} from "svelte";
+
+  const themes = ["light", "dark", "system"];
+  let current_theme_index = 0;
+
+  const handle_theme_click = (event: Event) => {
+    const button = (event.target as HTMLElement).closest(".theme-toggle");
+    current_theme_index = (current_theme_index + 1) % themes.length;
+    button?.setAttribute("data-theme", themes[current_theme_index]);
+  };
+
+  onMount(() => {
+    const button = document.querySelector(".theme-toggle");
+    button?.addEventListener("click", handle_theme_click);
+  });
+</script>
