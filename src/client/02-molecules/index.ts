74e43525481f26a4616399e453294731e7c6c4a7
oblivious <atha@fourth-wall.org>
2025-05-21 20:06:18 +0300

    .

--- src/client/02-molecules/index.ts
+++ src/client/02-molecules/index.ts
@@ -1,4 +1,4 @@
-export {default as BackButton} from "./BackButton";
+export {default as BackButton} from "./BackButton/BackButton.astro";
 export {default as CourseCard} from "./CourseCard.astro";
 export {default as Footer} from "./Footer.astro";
 export {default as Logo} from "./Logo.astro";
--- src/client/03-organisms/index.ts
+++ src/client/03-organisms/index.ts
@@ -1,5 +1,5 @@
 export {default as CourseHeader} from "./CourseHeader.astro";
-export {default as CourseList} from "./CourseList";
+export {default as CourseList} from "./CourseList/CourseList.astro";
 export {default as CourseSection} from "./CourseSection.astro";
 export {default as Head} from "./Head.astro";
 export {default as MenuBar} from "./MenuBar.astro";
