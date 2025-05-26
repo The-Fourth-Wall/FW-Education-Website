207beee27273a772108122ed89dd915b9f56aeb0
oblivious <atha@fourth-wall.org>
2025-05-26 20:20:47 +0300

    [FT-02] - added a user store that saves all user information.

--- src/client/01-atoms/buttons/ThemeSwitcher/ThemeSwitcherLogic.svelte
+++ src/client/01-atoms/buttons/ThemeSwitcher/ThemeSwitcherLogic.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  import {theme} from "@models";
+  import {user, type Theme} from "@models";
   import {onMount} from "svelte";
 
-  const themes = ["light", "dark", "system"];
-  let current_theme_index = themes.indexOf(theme.get());
+  const themes = ["light", "dark", "system"] as Theme[];
+  let current_theme_index = themes.indexOf(user.get()?.theme);
 
   const theme_switch = () => {
     current_theme_index = (current_theme_index + 1) % themes.length;
     const new_theme = themes[current_theme_index];
-    theme.set(new_theme);
+    user.set({...user.get(), theme: new_theme});
     document.documentElement.className = `${new_theme}-theme`;
   };
 
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,3 @@
 export * from "./course.model";
 export * from "./icon.model";
-export * from "./theme";
+export * from "./user";
