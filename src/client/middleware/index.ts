397b377639ef3f00299f0487515d02913dc91e47
oblivious <atha@fourth-wall.org>
2025-05-26 21:51:03 +0300

    [FT-02] - added middleware for handling sessions and user data.

---
+++ src/client/middleware/index.ts
@@ -0,0 +1,39 @@
+import {handle_session} from "@app/FT-02-authentication/session";
+import {defineMiddleware} from "astro:middleware";
+import micromatch from "micromatch";
+
+const redirect_routes = ["/login(|/)", "/register(|/)"];
+const protected_routes = ["/settings(|/)"];
+const hybrid_routes = ["/"];
+
+export const onRequest = defineMiddleware(
+  async ({url, cookies, redirect}, next) => {
+    const access_token = cookies.get("sb-access-token")?.value;
+    const refresh_token = cookies.get("sb-refresh-token")?.value;
+
+    if (micromatch.isMatch(url.pathname, redirect_routes)) {
+      if (access_token && refresh_token) {
+        return redirect("/");
+      }
+    } else if (micromatch.isMatch(url.pathname, protected_routes)) {
+      if (!access_token || !refresh_token) {
+        return redirect("/login");
+      } else {
+        const {error} = await handle_session(cookies, {
+          access_token,
+          refresh_token,
+        });
+        if (error) {
+          return redirect("/login");
+        }
+      }
+    } else if (micromatch.isMatch(url.pathname, hybrid_routes)) {
+      await handle_session(cookies, {
+        access_token,
+        refresh_token,
+      });
+    }
+
+    return next();
+  },
+);
