0338caf5ab5f4c33c59e70f93c3c3a3907e0444f
oblivious <atha@fourth-wall.org>
2025-06-04 13:21:54 +0300

    [FT-09] - refactored to astro directory so i can add react, svelte and styles.

---
+++ src/client/01-atoms/astro/index.ts
@@ -0,0 +1,8 @@
+export {default as Badge} from "./Badge.astro";
+export {default as Border} from "./Border.astro";
+export {default as Divider} from "./Divider.astro";
+export {default as Icon} from "./Icon.astro";
+export {default as LinkButton} from "./LinkButton.astro";
+export {default as Ribbon} from "./Ribbon.astro";
+export {default as Spacer} from "./Spacer.astro";
+export {default as Text} from "./Text.astro";
