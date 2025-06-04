dc47ba22b8d8c40dec0462506c613c491d6a8466
oblivious <atha@fourth-wall.org>
2025-06-04 04:13:25 +0300

    [QOL-08] - completely redesigned structure following closely the atomic design.

--- src/client/01-atoms/index.ts
+++ src/client/01-atoms/index.ts
@@ -1,10 +1,8 @@
 export {default as Badge} from "./Badge/Badge.astro";
 export {default as Border} from "./Border/Border.astro";
 export {default as Divider} from "./Divider/Divider.astro";
-export {default as Form} from "./Form/Form.astro";
 export {default as Icon} from "./Icon/Icon.astro";
 export {default as LinkButton} from "./LinkButton/LinkButton.astro";
 export {default as Ribbon} from "./Ribbon/Ribbon.astro";
 export {default as Spacer} from "./Spacer/Spacer.astro";
 export {default as Text} from "./Text/Text.astro";
-export {default as ThemeSwitcher} from "./ThemeSwitcher/ThemeSwitcher.astro";
