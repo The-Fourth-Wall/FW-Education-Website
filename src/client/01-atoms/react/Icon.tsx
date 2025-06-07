632a25fbc4cfe631d5f865f1b2f1b12ab478e6c4
oblivious <atha@fourth-wall.org>
2025-06-08 01:00:13 +0300

    [master] - revamped theme switcher icons.

--- src/client/01-atoms/react/Icon.tsx
+++ src/client/01-atoms/react/Icon.tsx
@@ -43,15 +43,21 @@ export const Icon = ({icon, size}: Props) => {
       <svg
         style={{width: `${size}rem`, height: `${size}rem`}}
         xmlns="http://www.w3.org/2000/svg"
-        fill="none"
         viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="var(--fg-color)">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-        />
+        fill="none"
+        stroke="var(--fg-color)"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round">
+        <circle cx="12" cy="12" r="4" />
+        <path d="M12 2v2" />
+        <path d="M12 20v2" />
+        <path d="m4.93 4.93 1.41 1.41" />
+        <path d="m17.66 17.66 1.41 1.41" />
+        <path d="M2 12h2" />
+        <path d="M20 12h2" />
+        <path d="m6.34 17.66-1.41 1.41" />
+        <path d="m19.07 4.93-1.41 1.41" />
       </svg>
     );
   } else if (icon === "moon") {
@@ -59,15 +65,13 @@ export const Icon = ({icon, size}: Props) => {
       <svg
         style={{width: `${size}rem`, height: `${size}rem`}}
         xmlns="http://www.w3.org/2000/svg"
-        fill="none"
         viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="var(--fg-color)">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-        />
+        fill="none"
+        stroke="var(--fg-color)"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round">
+        <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
       </svg>
     );
   } else if (icon === "half-moon") {
@@ -75,15 +79,12 @@ export const Icon = ({icon, size}: Props) => {
       <svg
         style={{width: `${size}rem`, height: `${size}rem`}}
         xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
         viewBox="0 0 24 24"
         fill="none"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="lucide lucide-sun-moon-icon lucide-sun-moon">
+        stroke="var(--fg-color)"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round">
         <path d="M12 8a2.83 2.83 0 0 0 4 4 4 4 0 1 1-4-4" />
         <path d="M12 2v2" />
         <path d="M12 20v2" />
